0344318f1e019d7eac731289cca6cc4fab89abad
absence77 <axmed.qayibov@yandex.ru>
2023-02-21 21:12:00 +0100

    add source code

---
+++ src/index.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "Hello, World from Docker! <br>";
+echo "This application has been deployed via CI/CD Jenkins Pipeline! <br>";
+echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
+
+?>
