ae8cbc688a8b8710b56c7048998f83ac6d5cbbf1
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2023-02-21 20:57:54 +0100

    initial commit for source code

---
+++ src/index.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "Hello, World from Docker! <br>";
+echo "This docker image has been deployed by CI/CD pipeline";
+echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
+
+?>
