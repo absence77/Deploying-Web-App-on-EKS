68cd1c729a1935821e0ac66cafc8cc7b88437e59
Akhmed Gayibov <38292696+absence77@users.noreply.github.com>
2023-02-21 20:58:55 +0100

    Delete src directory

--- src/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-echo "Hello, World from Docker! <br>";
-echo "This docker image has been deployed by CI/CD pipeline";
-echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
-
-?>
